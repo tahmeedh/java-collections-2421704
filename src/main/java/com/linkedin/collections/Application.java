d99ba868e94eb8d4a64f1f28d262cb66cf5b72c4
tahmeedh <60047767+tahmeedh@users.noreply.github.com>
2023-07-05 18:38:04 -0700

    use java collections

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,8 +1,20 @@
-package com.linkedin.collections;
+// package com.linkedin.collections;
+
+import java.util.ArrayDeque;
+import java.util.*;
+import java.util.Collection;
 
 public class Application {
 
 	public static void main(String[] args) {
+		Collection<String> c = new ArrayDeque<>();
+		c.add("snowboard");
+		c.add("linux");
+		c.add("books");
+		c.add("snowboard");
+		
+		Collection<String> favoriteThings = new HashSet<>(c);
+		System.out.println(favoriteThings);
 		
 		
 	}
